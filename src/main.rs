3427ad94b7f1b4b0291e7511e6d033da293b27ea
Amirerfan Rafati <aerfanr@mailo.com>
2022-07-06 21:23:36 +0430

    Added form handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+use wasm_bindgen::JsCast;
+use yew::prelude::*;
+use web_sys::HtmlInputElement;
+
+enum Msg {
+    UpdateUsername(String),
+    UpdatePassword(String),
+    Submit
+}
+
+#[derive(Default)]
+struct Model {
+    result: String,
+    username: String,
+    password: String
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self::default()
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Msg) -> bool {
+        match msg {
+            Msg::UpdateUsername(e) => {
+                // web_sys::console::log_1(&format!("Username: {}", e).into());
+                self.username = e;
+                true
+            },
+            Msg:: UpdatePassword(e) => {
+                // web_sys::console::log_1(&format!("Password: {}", e).into());
+                self.password = e;
+                true
+            },
+            Msg::Submit => {
+                self.result = format!("Username: {}\nPassword: {}", self.username, self.password);
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        macro_rules! callback {
+            ( $t:expr ) => {
+                ctx.link().batch_callback(|e: InputEvent| {
+                    let target = e.target();
+                    let input = target.and_then(|target| target.dyn_into::<HtmlInputElement>().ok());
+                    input.map(|input| $t(input.value()))
+                })
+            }
+        }
+
+        let username_change = callback!(Msg::UpdateUsername);
+        let password_change = callback!(Msg::UpdatePassword);
+        let on_submit = ctx.link().callback(|e: FocusEvent| {
+            e.prevent_default();
+            Msg::Submit
+        });
+
+        html! {
+            <div>
+                <form onsubmit={on_submit}>
+                    <span>
+                        <label for="username"> {"Username:"} </label>
+                        <input oninput={username_change} id="username" type="text"/>
+                    </span>
+                    <span>
+                        <label for="password"> {"Password:"} </label>
+                        <input oninput={password_change} id="password" type="password"/>
+                    </span>
+                    <span>
+                        <input type="submit"/>
+                    </span>
+                </form>
+                <p> {&self.result} </p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
